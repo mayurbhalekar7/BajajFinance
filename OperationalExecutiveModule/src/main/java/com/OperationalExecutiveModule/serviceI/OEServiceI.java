e2931246ef9f40a05b305e3e947cd1f2e2128ce7
KajalRathod05 <kajalrathod0576@gmail.com>
2024-08-08 13:17:26 +0530

    Operational Executive Module Created By Kajal

--- OperationalExecutiveModule/src/main/java/com/OperationalExecutiveModule/serviceI/OEServiceI.java
+++ OperationalExecutiveModule/src/main/java/com/OperationalExecutiveModule/serviceI/OEServiceI.java
@@ -1,5 +1,19 @@
 package com.OperationalExecutiveModule.serviceI;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.OperationalExecutiveModule.model.Enquiry;
+
 public interface OEServiceI {
 
+	List<Enquiry> getAllEnquiries();
+
+	Optional<Enquiry> checkCibilScore(int enquiryId);
+
+	Optional<Enquiry> getByEmail(String email);
+
+	void forwordToRelationalExecutive(Enquiry e);
+
+	List<Enquiry> getEnquiry();
 }
